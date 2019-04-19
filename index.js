c046f279c88686b23d61efd15ea752bcf976e299
aayush6194 <aayush6194@gmail.com>
2019-04-19 02:24:45 -0500

    Put allowed, comments enabled

--- index.js
+++ index.js
@@ -7,8 +7,9 @@ const utils = require('./utils');
 
 /**
  * todo: logout, likes, friends
- */
+ */const cors = require('cors');
 
+ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
